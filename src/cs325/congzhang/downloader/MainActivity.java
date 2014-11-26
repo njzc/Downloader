ab082aaacaddee464d52db247a0fb2f9da456861
cong.zhang <cong.zhang@LW579Y.iperceptions.com>
2014-11-26 10:48:28 -0500

    MainActivity with Facebook Login

--- src/cs325/congzhang/downloader/MainActivity.java
+++ src/cs325/congzhang/downloader/MainActivity.java
@@ -1,17 +1,42 @@
 package cs325.congzhang.downloader;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	private UiLifecycleHelper uiHelper;
+	private static final String TAG = "MainActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// To maintain FB Login session
+		uiHelper = new UiLifecycleHelper(this, callback);
+		uiHelper.onCreate(savedInstanceState);
 	}
+
+	// Called when session changes
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,4 +56,61 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	// When session is changed, this method is called from callback method
+	private void onSessionStateChange(Session session, SessionState state,
+			Exception exception) {
+		// When Session is successfully opened (User logged-in)
+		if (state.isOpened()) {
+			Log.i(TAG, "Logged in...");
+			// make request to the /me API to get Graph user
+			Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+				// callback after Graph API response with user
+				// object
+				@Override
+				public void onCompleted(GraphUser user, Response response) {
+					if (user != null) {
+					}
+				}
+			}).executeAsync();
+		} else if (state.isClosed()) {
+			Log.i(TAG, "Logged out...");
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+		Log.i(TAG, "OnActivityResult...");
+		
+		
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		uiHelper.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+
 }
